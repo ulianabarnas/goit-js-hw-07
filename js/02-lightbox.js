d893a547b0cd627b9ee26daa1ca4d400494da1da
Uliana Vasylchenko <ulianabarnas9225@gmail.com>
2022-07-18 15:06:19 +0200

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = createImageGalleryMarkup(galleryItems);
+
+galleryContainer.innerHTML = imagesMarkup;
+
+function createImageGalleryMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  scrollZoom: false,
+});
